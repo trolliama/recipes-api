b5d256fc1adee31027aad2bca96aa0dac36afe79
trolliama <caiow2001@gmail.com>
2021-06-02 17:52:37 -0300

    Added token user creation

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # externals
+    "rest_framework",
+    "rest_framework.authtoken",
     # locals
     "core",
+    "user",
 ]
 
 MIDDLEWARE = [
